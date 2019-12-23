0ae2ce2221c4e37e4f330b76f21c0ff86a5331d3
Wolf Derrick <oscar-rong@hotmail.com>
2019-12-23 23:01:11 +0000

    Finish mark red and print people that should be kicked.

--- main.py
+++ main.py
@@ -34,6 +34,19 @@ def fillRed(ws, top_length, row):
         ws.cell(row=row, column=1).fill = red_fill
         ws.cell(row=row, column=2).fill = red_fill
 
+def should_kick(ws, top_length, row):
+    should_kick = True
+    for i in range(top_length, top_length - 8, -1):
+        if ws.cell(row=row, column=i).value or (
+                ws.cell(row=row, column=i).fill.start_color.rgb == '00000000' and not ws.cell(row=row,
+                                                                                              column=i).fill.patternType is None):
+            should_kick = False
+            break
+    if not should_kick:
+        for i in range(top_length, 4, -1):
+            if ws.cell(row=row, column=i).value:
+                return False
+    return True
 
 def fill_yellow(ws, top_length, row):
     should_fill_yellow = True
@@ -43,12 +56,42 @@ def fill_yellow(ws, top_length, row):
     for i in range(top_length, top_length - 5, -1):
         if ws.cell(row=row, column=i).value or (
                 ws.cell(row=row, column=i).fill.start_color.rgb == '00000000' and not ws.cell(row=row,
-                                                                                        column=i).fill.patternType is None):
+                                                                                              column=i).fill.patternType is None):
             should_fill_yellow = False
             break
     if should_fill_yellow:
         ws.cell(row=row, column=1).fill = yellow_fill
         ws.cell(row=row, column=2).fill = yellow_fill
+
+
+def mark_consecutive_four_goals_achieved(achievement_list):
+    achieved_counter = 0
+    total_allowed_counter = 0
+    start_indexes = []
+    for i in range(len(achievement_list)):
+        if achievement_list[i] == '达标':
+            achieved_counter += 1
+        else:
+            achieved_counter = 0
+        if achieved_counter == 4 and total_allowed_counter <= 2:
+            start_indexes.append(i - 3)
+            total_allowed_counter += 1
+    return start_indexes
+
+
+def fill_green(ws, row, achievement_list):
+    consecutive_four_indexes = mark_consecutive_four_goals_achieved(achievement_list)
+    green_fill = xl.styles.PatternFill(start_color='FF92D050',
+                                       end_color='FF92D050',
+                                       fill_type='solid')
+    if len(consecutive_four_indexes):
+        ws.cell(row=row, column=1).fill = green_fill
+        ws.cell(row=row, column=2).fill = green_fill
+        for i in range(len(consecutive_four_indexes)):
+            for j in range(4):
+                ws.cell(row=row, column=6 + consecutive_four_indexes[i] + j).fill = green_fill
+
+    pass
 
 
 if __name__ == '__main__':
@@ -61,17 +104,22 @@ if __name__ == '__main__':
     all_end_date = []
     top_row = ws[1]
     top_length = len(top_row)
-    print(ws['O58'].fill)
+    row_count = ws.max_row
     for x in range(6, top_length + 1):
         all_end_date.append(getDateFromCell(ws.cell(row=1, column=x), date_format1))
-    for x in range(2, 59):
+    for x in range(2, row_count+1):
         date_of_entering = ws['C%d' % x].value
         current_date = datetime.now()
-
         fillBlack(ws, date_of_entering, all_end_date)
+        achievement_list = [i.value for i in ws[x][5:]]
+        fill_green(ws, x, achievement_list)
         fill_yellow(ws, top_length, x)
-        # print(ws['L54'].fill.start_color.rgb)
         if (current_date - date_of_entering).days <= 30:
             fillRed(ws, top_length, x)
-    print(ws['O58'].fill)
+        else:
+            if should_kick(ws, top_length, x):
+                print(ws.cell(row=x, column=2).value)
     wb.save('Sample.xlsx')
+#     TODO: Return all QQ/username of people who will be kicked out.
+#     1. All empty, Now - Current > 30 days
+#     2. Start from last, 8 consecutive empty.
