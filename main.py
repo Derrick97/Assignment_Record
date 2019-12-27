a9470aeb0856da5d114dd29457d9976cb7a6ef07
Wolf Derrick <oscar-rong@hotmail.com>
2019-12-27 22:31:46 +0000

    Refactor function name.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def getDateFromCell(cell, format):
     return date
 
 
-def fillBlack(ws, date_of_entering, all_end_date):
+def fill_black(ws, date_of_entering, all_end_date):
     start_col = 6
     black_fill = xl.styles.PatternFill(start_color='00000000',
                                        end_color='00000000',
@@ -21,7 +21,7 @@ def fillBlack(ws, date_of_entering, all_end_date):
         start_col += 1
 
 
-def fillRed(ws, top_length, row):
+def fill_red(ws, top_length, row):
     should_fill_red = True
     red_fill = xl.styles.PatternFill(start_color='FFFF0000',
                                      end_color='FFFF0000',
@@ -33,6 +33,7 @@ def fillRed(ws, top_length, row):
     if should_fill_red:
         ws.cell(row=row, column=1).fill = red_fill
         ws.cell(row=row, column=2).fill = red_fill
+
 
 def should_kick(ws, top_length, row):
     should_kick = True
@@ -47,6 +48,7 @@ def should_kick(ws, top_length, row):
             if ws.cell(row=row, column=i).value:
                 return False
     return True
+
 
 def fill_yellow(ws, top_length, row):
     should_fill_yellow = True
@@ -107,15 +109,15 @@ if __name__ == '__main__':
     row_count = ws.max_row
     for x in range(6, top_length + 1):
         all_end_date.append(getDateFromCell(ws.cell(row=1, column=x), date_format1))
-    for x in range(2, row_count+1):
+    for x in range(2, row_count + 1):
         date_of_entering = ws['C%d' % x].value
         current_date = datetime.now()
-        fillBlack(ws, date_of_entering, all_end_date)
+        fill_black(ws, date_of_entering, all_end_date)
         achievement_list = [i.value for i in ws[x][5:]]
         fill_green(ws, x, achievement_list)
         fill_yellow(ws, top_length, x)
         if (current_date - date_of_entering).days <= 30:
-            fillRed(ws, top_length, x)
+            fill_red(ws, top_length, x)
         else:
             if should_kick(ws, top_length, x):
                 print((ws.cell(row=x, column=1).value, ws.cell(row=x, column=2).value))
